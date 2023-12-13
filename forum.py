22eefc79198654e1d47f1a913aac0b33bcd29af8
Lena Araújo <lenaaraujo92@gmail.com>
2023-12-12 21:45:42 -0300

    login route - html

--- forum.py
+++ forum.py
@@ -4,35 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Home
-@app.route("/")
-@app.route("/login")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
 # Login
-@app.route("/forum")
-def forum():
-    return render_template("forum.html")
-
-# Sintaxe para infos dinâmicas
-# @app.route('/reporter/<int:reporter_id>')
-# def reporter(reporter_id):
-#     return f'''
-#     <h2>Reporter {reporter_id} Bio</h2>
-#     <a href="/">Return to home page</a>
-#     '''
-
-# @app.route('/article/<article_name>')
-# def article(article_name):
-#   return f'''
-#   <h2>{article_name.replace('-', ' ').title()}</h2>
-#   <a href="/">Return back to home page</a>
-#   '''
-
-# Usuários
-# @app.route("/usuarios/<nome_usuario>")
-# def usuarios(nome_usuario):
-#     return render_template("usuarios.html", nome_usuario=nome_usuario)
+# modifiquei user_id=user_id. deixar assim até poder vincular ao banco de dados
+@app.route("/forum/<user_id>", methods=["GET", "POST"])
+def forum(user_id):
+    return render_template("forum.html", user_id=user_id)
 
 # Run
 if __name__ == "__main__":
